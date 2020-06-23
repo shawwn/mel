cde730287aca1ebc825a6aac11811a69b89b8ad2
Shawn Presser <shawnpresser@gmail.com>
2020-06-23 01:56:19 -0700

    Initial utilities

---
+++ mel/__init__.py
@@ -0,0 +1,302 @@
+import numpy as np
+
+
+import tensorflow
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import tensor_util
+
+
+try:
+  import torch
+except ModuleNotFoundError:
+  torch = None
+
+
+def is_lib_type(value, libname):
+  name = type(value).__module__
+  if name == libname:
+    return True
+  if '.' in name:
+    return name.split('.')[0] == libname
+  return False
+
+
+def is_tf_type(value):
+  return is_lib_type(value, 'tensorflow')
+  # if tensorflow.is_tensor(value):
+  #   return True
+  # if isinstance(value, tensorflow.Variable):
+  #   return True
+  # if isinstance(value, tensorflow.TensorShape):
+  #   return True
+  # if isinstance(value, tensorflow.Dimension):
+  #   return True
+  # return False
+
+
+def is_torch_type(value):
+  return is_lib_type(value, 'torch')
+  # if torch is None:
+  #   return False
+  # if torch.is_tensor(value):
+  #   return True
+  # return False
+
+
+def is_np_type(value):
+  return type(value).__module__ == 'numpy'
+  # for k, v in np.typeDict.items():
+  #   if isinstance(value, v):
+  #     return True
+  # return False
+
+
+def pynum(value):
+  return isinstance(value, float) or isinstance(value, int)
+
+
+def pybool(value):
+  return value is True or value is False
+
+
+def pystr(value):
+  return isinstance(value, str) or isinstance(value, bytes)
+
+
+def pynil(value):
+  return value is None
+
+
+def pyatom(value):
+  return pynum(value) or pybool(value) or pystr(value) or pynil(value) or is_np_type(value)
+
+
+def pylist(value):
+  return isinstance(value, list)
+
+
+def pydict(value):
+  return isinstance(value, dict)
+
+
+def pyobj(value):
+  return pylist(value) or pydict(value)
+
+
+def is_np(value, strict=False, deep=True):
+  if isinstance(value, np.ndarray):
+    return True
+  if not strict and pyatom(value):
+    return True
+  if deep and pylist(value):
+    return all([is_np(x, strict=strict, deep=deep) for x in value])
+  if deep and pydict(value):
+    return all([is_np(v, strict=strict, deep=deep) for k, v in value.items()])
+  return False
+
+
+def is_tf(value, strict=False, deep=True):
+  if tensorflow.is_tensor(value):
+    return True
+  if isinstance(value, tensorflow.Variable):
+    return True
+  if isinstance(value, tensorflow.TensorShape):
+    return True
+  if isinstance(value, tensorflow.Dimension):
+    return True
+  if not strict and pyatom(value):
+    return True
+  if deep and pylist(value):
+    return all([is_tf(x, strict=strict, deep=deep) for x in value])
+  if deep and pydict(value):
+    return all([is_tf(v, strict=strict, deep=deep) for k, v in value.items()])
+  return False
+
+
+def is_tf_variable(value):
+  if isinstance(value, tensorflow.Variable):
+    return True
+  return False
+
+
+def is_torch_tensor(value):
+  if torch is None:
+    return False
+  if torch.is_tensor(value):
+    return True
+  return False
+
+
+def is_tf_constant(value):
+  try:
+    return constant_op.is_constant(value)
+  except AttributeError:
+    # TODO: is there a cleaner way to accomplish this?
+    return False
+
+
+def val(value, eager=False, session=None, deep=True):
+  if isinstance(value, tensorflow.TensorShape):
+    value = value.as_list()
+  if isinstance(value, tensorflow.Dimension):
+    value = value.value
+  if torch:
+    if isinstance(value, torch.Size):
+      value = list(value)
+  #if is_tf(value, strict=True) and not pyobj(value) and constant_op.is_constant(value):
+  if is_tf_constant(value):
+    value = tensor_util.constant_value(value)
+  if deep and pylist(value):
+    value = [val(x, eager=eager, session=session, deep=deep) for x in value]
+  if deep and pydict(value):
+    value = {k: val(v, eager=eager, session=session, deep=deep) for k, v in value.items()}
+  if is_tf_variable(value):
+    if not eager:
+      raise ValueError("val called on tensorflow variable {} but eager is False".format(value))
+    # TODO: batch multiple nested reads.
+    result = value.eval(session=session)
+    return val(result, eager=eager, session=session, deep=deep)
+  if is_torch_tensor(value):
+    if not eager:
+      raise ValueError("val called on torch tensor {} but eager is False".format(value))
+    result = value.numpy()
+    return val(result, eager=eager, session=session, deep=deep)
+  if not is_np(value):
+    raise ValueError("val called with value of unknown type: {}".format(value))
+  return value
+
+
+import importlib
+
+
+def as_lib(lib=None, hint=None):
+  if isinstance(lib, str):
+    if lib == 'tf':
+      lib = 'tensorflow'
+    if lib == 'np':
+      lib = 'numpy'
+    if isinstance(lib, str):
+      if lib == 'tensorflow':
+        lib = tensorflow
+      elif lib == 'numpy':
+        lib = np
+      elif lib == 'torch':
+        lib = torch
+      else:
+        lib = importlib.__import__(lib)
+      return lib
+  if lib is None:
+    if is_tf_type(hint):
+      return tensorflow
+    if is_torch_type(hint):
+      return torch
+    return np
+  assert lib is tensorflow or lib is np or lib is torch
+  return lib
+
+
+dtype_remap = {
+    'bool': 'b',
+
+    'f': 'f32',
+    'float': 'f32',
+    'float32': 'f32',
+
+    'd': 'f64',
+    'double': 'f64',
+    'float64': 'f64',
+
+    'c': 'f128',
+    'complex': 'f128',
+    'complex128': 'f128',
+
+    'byte': 'u8',
+    'uint8': 'u8',
+    'ushort': 'u16',
+    'uint16': 'u16',
+    'uint': 'u32',
+    'ulong': 'u64',
+    'ulong64': 'u64',
+
+    'u': 'u32',
+    'unsigned': 'u32',
+    'uint32': 'u32',
+
+    'int8': 'i8',
+
+    'short': 'i16',
+    'int16': 'i16',
+
+    'i': 'i32',
+    'signed': 'i32',
+    'int': 'i32',
+    'int32': 'i32',
+
+    'l': 'i64',
+    'long': 'i64',
+    'long64': 'i64',
+    'resource': 'r',
+    'variant': 'v',
+}
+
+
+for v in list(dtype_remap.values()):
+  dtype_remap[v] = v
+
+
+dtype_long = {
+    'b': 'bool',
+    'f32': 'float32',
+    'f64': 'float64',
+    'f128': 'complex',
+    'u8': 'uint8',
+    'u16': 'uint16',
+    'u32': 'uint32',
+    'u64': 'uint64',
+    'i8': 'int8',
+    'i16': 'int16',
+    'i32': 'int32',
+    'i64': 'int64',
+    'r': 'resource',
+    'v': 'variant',
+}
+
+
+for v in list(dtype_remap.values()):
+  assert v in dtype_long
+
+
+def as_dtype(value, lib=None, deep=True):
+  lib = as_lib(lib, hint=value)
+  if isinstance(value, str):
+    if value not in dtype_remap:
+      raise ValueError("Unknown dtype spec: {}".format(value))
+    value = dtype_remap[value]
+    assert value in dtype_long
+    value = dtype_long[value]
+    if not hasattr(lib, value):
+      raise ValueError("Library {} does not support dtype {}".format(lib, value))
+    value = getattr(lib, value)
+  if hasattr(value, 'dtype'):
+    value = value.dtype
+  if deep and pylist(value):
+    value = [as_dtype(x, lib=lib, deep=deep) for x in value]
+  if deep and pydict(value):
+    value = {k: as_dtype(v, lib=lib, deep=deep) for k, v in value.items()}
+  return value
+
+
+from . import tf
+
+
+# from . import _version
+# __version__ = _version.__version__
+
+
+# Version info -- read without importing
+import os
+_locals = {}
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(__dir__, "_version.py")) as fp:
+  exec(fp.read(), None, _locals)
+__version__ = _locals["__version__"]
---
+++ mel/tf.py
@@ -0,0 +1,241 @@
+import tensorflow as tf
+import numpy as np
+import functools
+
+
+def op_scope(fn, name=None):
+    if name is None:
+        name = fn.__name__
+    @functools.wraps(fn)
+    def _fn(*args, **kwargs):
+        with tf.name_scope(fn.__name__):
+            return fn(*args, **kwargs)
+    return _fn
+
+
+def pynum(u):
+  return isinstance(u, float) or isinstance(u, int)
+
+
+@op_scope
+def clamp(v, min=0., max=1.):
+  if pynum(v):
+    return np.clip(v, min, max)
+  else:
+    return tf.clip_by_value(v, min, max)
+
+
+@op_scope
+def wrap(v, wrap_mode="reflect"):
+  assert wrap_mode in ["clamp", "wrap", "reflect"]
+  if wrap_mode == "wrap":
+    return tf.math.floormod(v, 1.0)
+  elif wrap_mode == "reflect":
+    return tf.abs(tf.math.floormod(v, 2.0) - 1.0)
+  elif wrap_mode == "clamp":
+    return clamp(v)
+
+
+@op_scope
+def iround(u):
+  if pynum(u):
+    return u // 1.0
+  else:
+    return i32(tf.math.floordiv(f32(u), 1.0))
+
+
+@op_scope
+def lsh(u, by):
+  if pynum(u) and pynum(by):
+    return int(u) << by
+  else:
+    return tf.bitwise.left_shift(u, by)
+
+
+@op_scope
+def rsh(u, by):
+  if pynum(u) and pynum(by):
+    return int(u) >> by
+  else:
+    return tf.bitwise.right_shift(u, by)
+
+@op_scope
+def sign(u):
+  if pynum(u):
+    return np.sign(u)
+  else:
+    return tf.sign(u)
+
+@op_scope
+def min2(a, b):
+  if pynum(a) and pynum(b):
+    return min(a, b)
+  else:
+    return tf.minimum(a, b)
+
+
+@op_scope
+def max2(a, b):
+  if pynum(a) and pynum(b):
+    return max(a, b)
+  else:
+    return tf.maximum(a, b)
+
+
+@op_scope
+def min3(a, b, c):
+  if pynum(a) and pynum(b) and pynum(c):
+    return min(a, b, c)
+  else:
+    return tf.minimum(a, tf.minimum(b, c))
+
+
+@op_scope
+def max3(a, b, c):
+  if pynum(a) and pynum(b) and pynum(c):
+    return max(a, b, c)
+  else:
+    return tf.maximum(a, tf.maximum(b, c))
+
+
+@op_scope
+def f32(u):
+  if pynum(u):
+    return float(u)
+  else:
+    return tf.cast(u, tf.float32)
+
+
+@op_scope
+def i32(u):
+  if pynum(u):
+    return int(u)
+  else:
+    return tf.cast(u, tf.int32)
+
+
+@op_scope
+def sample(tex, uv, method="bilinear", wrap_mode="reflect"):
+  assert method in ["nearest", "bilinear"]
+  wh = tf.shape(tex[:, :, 0])
+  #gather_op = tf.gather_nd
+  gather_op = tf.raw_ops.GatherNd # use tf.raw_ops.GatherNd directly, to support resource variables inside a TPU computation
+  get = lambda u, v: gather_op(params=tex, indices=tf.stack([
+    clamp(iround(u), 0, wh[0] - 1),
+    clamp(iround(v), 0, wh[1] - 1),
+    ], 1))
+  uv = wrap(uv, wrap_mode)
+  u = uv[:, 0]
+  v = uv[:, 1]
+  u *= f32(wh)[0]
+  v *= f32(wh)[1]
+  if method == "nearest":
+    return get(u, v)
+  elif method == "bilinear":
+    # https://github.com/pytorch/pytorch/blob/f064c5aa33483061a48994608d890b968ae53fb5/aten/src/THNN/generic/SpatialGridSamplerBilinear.c#L105
+    ix = u - 0.5
+    iy = v - 0.5
+
+    # get NE, NW, SE, SW pixel values from (x, y)
+    ix_nw = iround(ix)
+    iy_nw = iround(iy)
+    ix_ne = ix_nw + 1
+    iy_ne = iy_nw
+    ix_sw = ix_nw
+    iy_sw = iy_nw + 1
+    ix_se = ix_nw + 1
+    iy_se = iy_nw + 1
+
+    sub = lambda a, b: f32(a) - f32(b)
+
+    # get surfaces to each neighbor:
+    nw = sub(ix_se , ix)    * sub(iy_se , iy);
+    ne = sub(ix    , ix_sw) * sub(iy_sw , iy);
+    sw = sub(ix_ne , ix)    * sub(iy    , iy_ne);
+    se = sub(ix    , ix_nw) * sub(iy    , iy_nw);
+
+    nw_val = f32(get(ix_nw, iy_nw))
+    ne_val = f32(get(ix_ne, iy_ne))
+    sw_val = f32(get(ix_sw, iy_sw))
+    se_val = f32(get(ix_se, iy_se))
+
+    a = lambda x: x[:, tf.newaxis]
+    out = nw_val * a(nw)
+    out += ne_val * a(ne)
+    out += sw_val * a(sw)
+    out += se_val * a(se)
+    return out
+
+
+@op_scope
+def resize(img, size, preserve_aspect_ratio=False, method="area", wrap_mode="reflect"):
+  assert method in ["nearest", "bilinear", "area"]
+  assert wrap_mode in ["clamp", "wrap", "reflect"]
+  y, x = tf.meshgrid(
+      tf.range(0.0, size[0] + 0.0),
+      tf.range(0.0, size[1] + 0.0))
+  num_frags = tf.reduce_prod(tf.shape(x))
+  uv = tf.stack([
+    #1.0 - tf.reshape(y, [-1]) / f32(size[1]),
+    #tf.reshape(x, [-1]) / f32(size[0]),
+
+    #1.0 - tf.reshape(y, [-1]) / f32(size[1]),
+    #tf.reshape(x, [-1]) / f32(size[0]),
+
+    tf.reshape(y, [-1]) / f32(size[0]),
+    tf.reshape(x, [-1]) / f32(size[1]),
+    #tf.zeros([num_frags], dtype=tf.float32)
+    ], axis=1)
+
+  re = lambda out: tf.transpose(tf.reshape(out, [size[1], size[0], 3]), [1,0,2])
+  if method == "nearest" or method == "bilinear":
+    return re(sample(img, uv, method=method, wrap_mode=wrap_mode))
+
+  uv_00 = uv
+  uv_10 = tf.stack([
+    uv_00[:, 0] + 1.0 / size[0],
+    uv_00[:, 1] + 0.0 / size[1],
+    ], axis=1)
+  uv_01 = tf.stack([
+    uv_00[:, 0] + 0.0 / size[0],
+    uv_00[:, 1] + 1.0 / size[1],
+    ], axis=1)
+  uv_11 = tf.stack([
+    uv_00[:, 0] + 1.0 / size[0],
+    uv_00[:, 1] + 1.0 / size[1],
+    ], axis=1)
+  wh = f32(tf.shape(img[:, :, 0]))
+
+  UV_00 = tf.reduce_min([uv_00, uv_10, uv_01, uv_11], axis=0)
+  UV_11 = tf.reduce_max([uv_00, uv_10, uv_01, uv_11], axis=0)
+  UV_01 = tf.stack([UV_00[:, 0], UV_11[:, 1]], 1)
+  UV_10 = tf.stack([UV_11[:, 0], UV_00[:, 1]], 1)
+
+  img_sum = tf.cumsum(tf.cumsum(f32(img), 0), 1)
+
+  tex_00 = f32(sample(img_sum, UV_00, "bilinear"))
+  tex_10 = f32(sample(img_sum, UV_10, "bilinear"))
+  tex_01 = f32(sample(img_sum, UV_01, "bilinear"))
+  tex_11 = f32(sample(img_sum, UV_11, "bilinear"))
+
+  R = wh[0]*(uv_11[:, 0] - uv_00[:, 0]) * wh[1]*(uv_11[:, 1] - uv_00[:, 1])
+
+  out = re(tex_11 - tex_10 - tex_01 + tex_00) / R[0]
+  out = clamp(out, 0.0, 255.0)
+  return out
+
+
+if __name__ == "__main__":
+  import sys
+  args = sys.argv[1:]
+  sess = tf.InteractiveSession()
+  with open(args[0], 'rb') as f:
+    img = sess.run(tf.io.decode_image(f.read(), channels=3))
+  outfile = args[1]
+  w = 20 if len(args) <= 2 else int(args[2])
+  h = 20 if len(args) <= 3 else int(args[3])
+  method = "area" if len(args) <= 4 else args[4]
+  wrap_mode = "reflect" if len(args) <= 5 else args[5]
+  img2 = sess.run(resize(img, [w, h], method=method, wrap_mode=wrap_mode))
+  with open(args[1], 'wb') as f:
+    f.write(sess.run(tf.image.encode_png(img2)))
